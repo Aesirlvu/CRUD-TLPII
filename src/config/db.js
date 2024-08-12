bba2e39cae3186be8637cb33a80c9693ebec24be
Ramiro_sanchezzz <ramiroaanchez345@gmail.com>
2024-08-12 13:04:49 -0300

    try-catch

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-// exportamos la función connectDB que se encargará de realizar la conexión a la base de datos.
 export const connectDB = async () => {
+try {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -10,4 +10,8 @@ export const connectDB = async () => {
     password: process.env.DB_PASSWORD,
   });
   return connection;
+} catch (err) {
+  res.status(500).send("Error al conectar con la Base de Datos..")
+  process.exit(1)
+}
 };
--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -1,130 +1,145 @@
 import { connectDB } from "../config/db.js";
 
-// Creamos y exportamos la funcion getTasks que se encargará de obtener todas las tareas de la base de datos.
 export const getTasks = async (req, res) => {
   const connection = await connectDB();
-  const query = `SELECT * FROM tasks`;
-  const [result] = await connection.query(query);
+  try {
+    const query = `SELECT * FROM tasks`;
+    const [result] = await connection.query(query);
 
-  if (!result.length) {
-    res.status(404).send("No hay tareas registradas.");
+    if (!result.length) {
+      res.status(404).send("No hay tareas registradas.");
+    }
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send("Error, no se pudo obtener las tareas.");
   }
-
-  res.status(200).json(result);
 };
 
-// Obtenemos una tarea por su id y la exportamos, ejecutando la consulta en la base de datos.
 export const getOneTask = async (req, res) => {
   const connection = await connectDB();
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const query = `
-  SELECT * FROM tasks
-  WHERE id = ?`;
+    const query = `
+    SELECT * FROM tasks
+    WHERE id = ?`;
 
-  const [result] = await connection.query(query, [id]);
-  console.log(result);
+    const [result] = await connection.query(query, [id]);
+    console.log(result);
 
-  if (!result.length) {
-    return res.status(404).send("No se encontró la tarea");
+    if (!result.length) {
+      return res.status(404).send("No se encontró la tarea");
+    }
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send("Error, no se pudo obtener la tarea.");
   }
-
-  res.status(200).json(result);
 };
 
-// la función createTask valida los campos isComplete, title y description, y luego inserta la tarea en la base de datos.
 export const createTask = async (req, res) => {
   const connection = await connectDB();
-  const { title, description, isComplete } = req.body;
+  try {
+    const { title, description, isComplete } = req.body;
 
-  if (isComplete !== 0 && isComplete !== 1) {
-    return res.status(400).send("El campo isComplete solo puede ser 0 o 1");
-  }
+    if (isComplete !== 0 && isComplete !== 1) {
+      return res.status(400).send("El campo isComplete solo puede ser 0 o 1");
+    }
 
-  const query = `
+    const query = `
     INSERT INTO tasks (title, description, isComplete)
     VALUES (?, ?, ?)`;
 
-  const checkQuery = `
+    const checkQuery = `
     SELECT * FROM tasks
     WHERE title = ?
     AND description = ?
     AND isComplete = ?`;
 
-  const [checkTask] = await connection.query(checkQuery, [
-    title,
-    description,
-    isComplete,
-  ]);
+    const [checkTask] = await connection.query(checkQuery, [
+      title,
+      description,
+      isComplete,
+    ]);
 
-  if (checkTask.length) {
-    return res.status(400).send("La tarea ya existe");
+    if (checkTask.length) {
+      return res.status(400).send("La tarea ya existe");
+    }
+
+    await connection.query(query, [title, description, isComplete]);
+    res.status(201).send("Tarea creada.");
+  } catch (error) {
+    res.status(500).send("Error inesperado al crear la tarea.");
   }
-
-  await connection.query(query, [title, description, isComplete]);
-  res.status(201).send("Tarea creada.");
 };
 
-// updateTask actualiza una tarea por su id, validando el campo isComplete y luego actualizando el registro en la base de datos.
 export const updateTask = async (req, res) => {
   const connection = await connectDB();
-  const { id } = req.params;
-  const { title, description, isComplete } = req.body;
+  try {
+    const { id } = req.params;
+    const { title, description, isComplete } = req.body;
 
-  if (isComplete !== 0 && isComplete !== 1) {
-    return res.status(400).send("El campo isComplete solo puede ser 0 o 1");
-  }
+    if (isComplete !== 0 && isComplete !== 1) {
+      return res.status(400).send("El campo isComplete solo puede ser 0 o 1");
+    }
 
-  const query = `
+    const query = `
     SELECT title, description, isComplete 
     FROM tasks 
     WHERE id = ?
     `;
 
-  const updateQuery = `
+    const updateQuery = `
     UPDATE tasks
     SET title = ?, description = ?, isComplete = ?
     WHERE id = ?`;
 
-  const [checkTask] = await connection.query(query, [id]);
+    const [checkTask] = await connection.query(query, [id]);
 
-  if (checkTask.length) {
-    return res.status(400).send("La tarea ya existe");
+    if (checkTask.length) {
+      return res.status(400).send("La tarea ya existe");
+    }
+
+    if (!checkTask.length) {
+      return res
+        .status(404)
+        .send("No se puede actualizar, no se encuentra el registro.");
+    }
+
+    await connection.query(updateQuery, [title, description, isComplete, id]);
+    res.status(200).send("Registro actualizado con éxito.");
+  } catch (error) {
+    res.status(500).send("Error inesperado al actualizar el registro.");
   }
-
-  if (!checkTask.length) {
-    return res
-      .status(404)
-      .send("No se puede actualizar, no se encuentra el registro.");
-  }
-
-  await connection.query(updateQuery, [title, description, isComplete, id]);
-  res.status(200).send("Registro actualizado con éxito.");
 };
 
-// deleteTask elimina una tarea por su id, validando si existe el registro y luego eliminándolo de la base de datos.
 export const deleteTask = async (req, res) => {
   const connection = await connectDB();
 
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const querySelect = `
+    const querySelect = `
   SELECT * FROM tasks
   WHERE id = ?`;
 
-  const queryDelete = `
+    const queryDelete = `
   DELETE FROM tasks
   WHERE id = ?
   `;
 
-  const [checkTask] = await connection.query(querySelect, [id]);
+    const [checkTask] = await connection.query(querySelect, [id]);
 
-  if (!checkTask.length) {
-    return res
-      .status(404)
-      .send("No se puede eliminar, no se encuentra el registro.");
+    if (!checkTask.length) {
+      return res
+        .status(404)
+        .send("No se puede eliminar, no se encuentra el registro.");
+    }
+
+    await connection.query(queryDelete, [id]);
+    res.status(200).send("Tarea eliminada.");
+  } catch (error) {
+    res.status(500).send("Error inesperado al eliminar la tarea.");
   }
-
-  await connection.query(queryDelete, [id]);
-  res.status(200).send("Tarea eliminada.");
 };
