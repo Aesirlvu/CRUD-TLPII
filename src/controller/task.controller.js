f82ea70dba3a43f3772e54c8165f4aa8a241d64f
Aesirlvu <you@example.com>
2024-08-14 19:36:31 -0300

    -

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -5,14 +5,14 @@ export const getTasks = async (req, res) => {
   try {
     const query = `SELECT * FROM tasks`;
     const [result] = await connection.query(query);
+    console.log(result);
 
     if (!result.length) {
-      res.status(404).send("No hay tareas registradas.");
+      return res.status(404).send("No hay tareas registradas.");
     }
-
-    res.status(200).json(result);
+    return res.status(200).json(result);
   } catch (error) {
-    res.status(500).send("Error, no se pudo obtener las tareas.");
+    res.status(400).send("Error, no se pudo obtener las tareas.");
   }
 };
 
@@ -29,48 +29,50 @@ export const getOneTask = async (req, res) => {
     console.log(result);
 
     if (!result.length) {
-      return res.status(404).send("No se encontró la tarea");
+      return res.status(404).send("No hay tarea.");
     }
-
-    res.status(200).json(result);
+    return res.status(200).json(result);
   } catch (error) {
-    res.status(500).send("Error, no se pudo obtener la tarea.");
+    res.status(400).send("Error, no se pudo obtener la tarea.");
   }
 };
 
 export const createTask = async (req, res) => {
   const connection = await connectDB();
   try {
-    const { title, description, isComplete } = req.body;
-
-    if (isComplete !== 0 && isComplete !== 1) {
-      return res.status(400).send("El campo isComplete solo puede ser 0 o 1");
-    }
-
+    const { title, description } = req.body;
+    // isComplete;
+    // if (isComplete !== 0 && isComplete !== 1) {
+    //   return res.status(400).send("El campo isComplete solo puede ser 0 o 1");
+    // }
+    // , isComplete
     const query = `
-    INSERT INTO tasks (title, description, isComplete)
-    VALUES (?, ?, ?)`;
+    INSERT INTO tasks (title, description)
+    VALUES (?, ?)`;
 
     const checkQuery = `
     SELECT * FROM tasks
     WHERE title = ?
     AND description = ?
-    AND isComplete = ?`;
+    `;
+    // AND isComplete = ?
 
     const [checkTask] = await connection.query(checkQuery, [
       title,
       description,
-      isComplete,
+      // isComplete,
     ]);
 
     if (checkTask.length) {
       return res.status(400).send("La tarea ya existe");
     }
 
-    await connection.query(query, [title, description, isComplete]);
-    res.status(201).send("Tarea creada.");
+    const result = await connection.query(query, [title, description]);
+    //  isComplete;
+
+    res.status(201).json(result);
   } catch (error) {
-    res.status(500).send("Error inesperado al crear la tarea.");
+    res.status(400).send("Error inesperado al crear la tarea.");
   }
 };
 
@@ -97,18 +99,20 @@ export const updateTask = async (req, res) => {
 
     const [checkTask] = await connection.query(query, [id]);
 
-    if (checkTask.length) {
-      return res.status(400).send("La tarea ya existe");
-    }
-
     if (!checkTask.length) {
       return res
         .status(404)
         .send("No se puede actualizar, no se encuentra el registro.");
     }
 
-    await connection.query(updateQuery, [title, description, isComplete, id]);
-    res.status(200).send("Registro actualizado con éxito.");
+    const result = await connection.query(updateQuery, [
+      title,
+      description,
+      isComplete,
+      id,
+    ]);
+
+    return res.status(200).json(result);
   } catch (error) {
     res.status(500).send("Error inesperado al actualizar el registro.");
   }
@@ -137,8 +141,8 @@ export const deleteTask = async (req, res) => {
         .send("No se puede eliminar, no se encuentra el registro.");
     }
 
-    await connection.query(queryDelete, [id]);
-    res.status(200).send("Tarea eliminada.");
+    const result = await connection.query(queryDelete, [id]);
+    return res.status(200).json(result);
   } catch (error) {
     res.status(500).send("Error inesperado al eliminar la tarea.");
   }
